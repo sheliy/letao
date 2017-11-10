e93d6f83ff4d0a8439cc70b75e951a2f582d878a
unknown <1793245518@qq.com>
2017-11-10 09:31:54 +0800

    首页静态页已完成

---
+++ public/back/js/common.js
@@ -0,0 +1,32 @@
+// 关闭进度条
+NProgress.configure({showSpinner: false});
+$(document).ajaxStart(function(){
+    NProgress.start();
+});
+$(document).ajaxStop(function(){
+    setTimeout(function(){
+        NProgress.done();
+    },500);
+});
+
+// 非登录页面发送ajax请求
+if(location.href.indexOf("login.html") == -1){
+    $.ajax({
+        type:"get",
+        url:"/employee/checkRootLogin",
+        success: function(data){
+            if(data.error === 400){
+                location.href = "login.html";
+            }
+        }
+    });
+}
+
+// 二级菜单显示隐藏
+$(".submenu").prev().on("click",function(){
+    $(this).next().slideToggle();
+});
+
+$(".btn_logout").on("click",function(){
+    $("#logoutModal").modal("show");
+});
--- public/back/js/index.js
+++ public/back/js/index.js
@@ -1,15 +1,8 @@
 $(function(){
-    /*1.注册人数  数据可视化*/
-    /*1.1准容器渲染图标*/
-    /*1.2准备数据 */
-    /*1.3引入核心echarts文件*/
-    /*1.4获取dom容器*/
-    var firstDom = document.querySelector('.picTable:first-child');
-    /*1.5初始化dom容器*/
-    var firstCarts = echarts.init(firstDom);
-    /*1.6进行配置和导入数据*/
+    var myChart = echarts.init(document.querySelector('.pic_left'));
+    
+     // 指定图表的配置项和数据
     var option = {
-        /*图标的标题*/
         title: {
             text: '2017年注册人数'
         },
@@ -18,22 +11,21 @@ $(function(){
             data:['人数']
         },
         xAxis: {
-            data: ["1月","2月","3月","4月","5月","6月"]
-        },
-        yAxis: {},
-        series: [{
-            name: '人数',
-            type: 'bar',
-            data: [1000, 2000, 3600, 1400, 1200, 2220]
-        }]
+            data: ["1月","2月","3月","4月","5月","6月"]},
+            yAxis: {},
+            series: [{
+                name: '人数',
+                type: 'bar',
+                data: [1000, 1499, 800, 2000, 1300, 1600]}]
     };
-    /*1.7 渲染图标*/
-    firstCarts.setOption(option);
+    
+            // 使用刚指定的配置项和数据显示图表。
+    myChart.setOption(option);
 
-    /*2.品牌销量  数据可视化*/
-    var secondDom = document.querySelector('.picTable:last-child');
-    var secondCarts = echarts.init(secondDom);
-    var secondOption = {
+
+    // 饼图
+    var myChart1 = echarts.init(document.querySelector('.pic_right'));
+    option = {
         title : {
             text: '热门品牌销售',
             subtext: '2017年6月',
@@ -41,25 +33,25 @@ $(function(){
         },
         tooltip : {
             trigger: 'item',
-            formatter: "{b} : {c} ({d}%)"
+            formatter: "{a} <br/>{b} : {c} ({d}%)"
         },
         legend: {
             orient: 'vertical',
             left: 'left',
-            data: ['耐克','阿迪','百伦','安踏','李宁']
+            data: ['阿迪','耐克','李宁','乔丹','安踏']
         },
         series : [
             {
-                name: '访问来源',
+                name: '销售情况',
                 type: 'pie',
                 radius : '55%',
                 center: ['50%', '60%'],
                 data:[
-                    {value:335, name:'耐克'},
-                    {value:310, name:'阿迪'},
-                    {value:234, name:'百伦'},
-                    {value:135, name:'安踏'},
-                    {value:1548, name:'李宁'}
+                    {value:335, name:'阿迪'},
+                    {value:310, name:'耐克'},
+                    {value:234, name:'李宁'},
+                    {value:135, name:'乔丹'},
+                    {value:1548, name:'安踏'}
                 ],
                 itemStyle: {
                     emphasis: {
@@ -71,5 +63,5 @@ $(function(){
             }
         ]
     };
-    secondCarts.setOption(secondOption);
-})
+    myChart1.setOption(option);
+});
